6dedd6c9bdec65cc728d0237f927eaa94cb3749c
Someone Somewhere <someone167@pm.me>
2022-10-22 01:50:25 +0100

    Add IsUnmanagedAttribute

---
+++ IsUnmanagedAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    sealed class IsUnmanagedAttribute : Attribute
+    {
+        // This is a positional argument
+        public IsUnmanagedAttribute()
+        {
+        }
+    }
+}
