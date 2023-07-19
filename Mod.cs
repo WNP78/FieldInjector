35424e1960080eb1790f53aade75b3b2f4200f3f
WNP78 <wnp78g@gmail.com>
2023-07-19 02:31:23 +0100

    Add inbuilt test case (Instantiate tests serialise then deserialsie)

--- Mod.cs
+++ Mod.cs
@@ -2,10 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using UnhollowerBaseLib;
-using UnhollowerRuntimeLib;
-using UnhollowerRuntimeLib.XrefScans;
 using UnityEngine;
 using Logging = MelonLoader.MelonLogger;
 
@@ -15,11 +11,39 @@ namespace FieldInjector
     {
         public override void OnInitializeMelon()
         {
-            SerialisationHandler.Inject<TestMBSt>(debugLevel: 5);
+            SerialisationHandler.Inject<TestMB8>(debugLevel: 5);
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            Test();
+        }
+
+        public static void Test()
+        {
+            Logging.Msg("Creating test object");
+            var g1 = new GameObject("Test Source");
+            var c = g1.AddComponent<TestMB8>();
+            c.space = Space.Self;
+            c.testB = AnisotropicFiltering.ForceEnable;
+            c.testEnum = TestMB8.TestEnum.B;
+            c.flagValue = 92;
+            c.tr = g1.transform;
+            c.array1 = new TestMB8.TestEnum[] { TestMB8.TestEnum.C, TestMB8.TestEnum.A, TestMB8.TestEnum.B };
+            c.spaces = new List<Space>(new Space[] { Space.World, Space.Self, Space.World });
+            c.stringArray = new string[] { "one", "two", "three" };
+            c.stringList = new List<string>(new string[] { "alpha", "bravo", "charlie" });
+            c.transformArray = new Transform[] { g1.transform, g1.transform };
+            c.objectList = new List<GameObject>(new GameObject[] { g1 });
+            c.testString = "tabloid's real name";
+
+            Logging.Msg("Duplicating test object\n\n\n");
+            UnityEngine.Object.Instantiate(c);
         }
     }
 
 }
+/*
 
 [Serializable]
 internal struct TestStruct
@@ -30,22 +54,30 @@ internal struct TestStruct
     public GameObject objRef;
 }
 
+
 internal class TestMBSt : MonoBehaviour
 {
     public TestStruct myStruct;
 
+#if !UNITY_EDITOR && !UNITY_2017_1_OR_NEWER
+    public TestMBSt(IntPtr ptr) : base(ptr) { }
+    static void Log(string s) => MelonLogger.Msg(s);
+#else
+    static void Log(string s) => Debug.Log(s);
+#endif
+
     void Awake()
     {
-        Logging.Msg($"myStruct.x = {this.myStruct.x}");
-        Logging.Msg($"myStruct.vector = {this.myStruct.vector.ToString()}");
-        Logging.Msg($"myStruct.str = {this.myStruct.str}");
-        Logging.Msg($"myStruct.objRef = {(this.myStruct.objRef == null ? "null" : this.myStruct.objRef.name)}");
+        Log($"myStruct.x = {this.myStruct.x}");
+        Log($"myStruct.vector = {this.myStruct.vector.ToString()}");
+        Log($"myStruct.str = {this.myStruct.str}");
+        Log($"myStruct.objRef = {(this.myStruct.objRef == null ? "null" : this.myStruct.objRef.name)}");
     }
 }
+*/
 
 
-/*
-internal class TestMB8 : MonoBehaviour
+public class TestMB8 : MonoBehaviour
 {
 #if !UNITY_EDITOR
     public TestMB8(IntPtr ptr) : base(ptr) { }
@@ -54,7 +86,7 @@ internal class TestMB8 : MonoBehaviour
     static void Log(string s) => Debug.Log(s);
 #endif
 
-    public enum TestEnum : byte
+    public enum TestEnum : int
     {
         A,
         B,
@@ -113,4 +145,4 @@ internal class TestMB8 : MonoBehaviour
         Log($"transformArray = {PrintObjArray(this.transformArray)}");
         Log($"objectList = {PrintObjArray(this.objectList.ToArray())}");
     }
-}*/
+}
