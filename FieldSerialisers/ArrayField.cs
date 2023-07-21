8a6b01e085e155948eacd08e1e4c03e2c5ce8e40
WNP78 <wnp78g@gmail.com>
2023-07-21 02:49:46 +0100

    Rework List and Array fields to use il2cpp arrays under the hood

--- FieldSerialisers/ArrayField.cs
+++ FieldSerialisers/ArrayField.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using MelonLoader;
+using System;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using UnhollowerBaseLib;
 using static FieldInjector.Util;
 using static MelonLoader.MelonLogger;
 using static UnhollowerBaseLib.IL2CPP;
@@ -14,6 +17,8 @@ namespace FieldInjector.FieldSerialisers
 
         protected Type _proxyType;
 
+        protected Type _elementType;
+
         public override int GetFieldSize(out int align)
         {
             align = sizeof(IntPtr);
@@ -22,6 +27,8 @@ namespace FieldInjector.FieldSerialisers
 
         protected ArrayField(Type elementType, FieldInfo field) : base(field)
         {
+            this._elementType = elementType;
+
             Log($"Creating array field for {field.FieldType.Name} {field.Name}", 5);
 
             if (elementType.IsEnum)
@@ -31,12 +38,32 @@ namespace FieldInjector.FieldSerialisers
 
             Log($"elementType = {elementType}", 5);
 
-            this._proxyType = typeof(ILCollections.List<>).MakeGenericType(elementType);
-            var classPtr = GetClassPointerForType(this._proxyType);
+            if (elementType.IsValueType)
+            {
+                this._proxyType = typeof(Il2CppStructArray<>).MakeGenericType(elementType);
+            }
+            else if (elementType == typeof(string))
+            {
+                this._proxyType = typeof(Il2CppStringArray);
+            }
+            else
+            {
+                this._proxyType = typeof(Il2CppReferenceArray<>).MakeGenericType(elementType);
+            }
 
-            Log($"ArrayField List<{elementType.Name}> ptr = {classPtr}", 5);
+            Log($"ProxyType = {this._proxyType}", 5);
 
-            this._fieldType = il2cpp_class_get_type(classPtr);
+            var classPtr = GetClassPointerForType(elementType);
+
+            Log($"classPtr = {classPtr}", 5);
+
+            var arrayClassPtr = il2cpp_array_class_get(classPtr, 1);
+
+            Log($"arrayClassPtr = {arrayClassPtr}", 5);
+
+            this._fieldType = il2cpp_class_get_type(arrayClassPtr);
+            
+            Log($"fieldType = {this._fieldType}", 5);
         }
 
         public ArrayField(FieldInfo field) : this(field.FieldType.GetElementType(), field)
@@ -47,7 +74,26 @@ namespace FieldInjector.FieldSerialisers
 
         protected override Expression GetManagedToNativeExpression(Expression managedObj)
         {
-            return ConvertArrayToIl2CppList(managedObj, this._proxyType);
+            Expression cppArray;
+            var managedElementType = this._elementType;
+            if (this._proxyType == typeof(Il2CppStringArray))
+            {
+                cppArray = Expression.New(this._proxyType.GetConstructor(new Type[] { typeof(string[]) }), managedObj);
+            }
+            else if (managedElementType.IsValueType)
+            {
+                Type cppElementType = this._proxyType.BaseType.GetGenericArguments()[0];
+
+                // only supports unmanaged structs
+                cppArray = Expression.Call(((Func<float[], Il2CppStructArray<float>>)ConvertArray<float, float>).Method.GetGenericMethodDefinition().MakeGenericMethod(managedElementType, cppElementType), managedObj);
+            }
+            else
+            {
+                Msg($"managedElementType = {managedElementType}, _proxyType = {this._proxyType}");
+                cppArray = Expression.New(this._proxyType.GetConstructor(new Type[] { managedElementType.MakeArrayType() }), managedObj);
+            }
+
+            return Expression.Property(cppArray, "Pointer");
         }
 
         protected override Expression GetNativeToManagedExpression(Expression nativePtr)
@@ -55,124 +101,60 @@ namespace FieldInjector.FieldSerialisers
             var ctor = this._proxyType.GetConstructor(new Type[] { typeof(IntPtr) });
             Expression cppList = Expression.New(ctor, nativePtr);
 
-            return ConvertListToManaged(cppList, this.field.FieldType);
+            return ConvertArrayToManaged(cppList, this._elementType.MakeArrayType());
         }
 
-        public static Expression ConvertListToManaged(Expression cppList, Type managedType)
+        public static Expression ConvertArrayToManaged(Expression cppArray, Type managedType)
         {
             if (!managedType.IsArray) { throw new ArgumentException("managedType is not an array!"); }
-            Type managedElementType = managedType.GetElementType();
+            // cppArray is of type _proxyType, managedtype is an array
+            var managedElementType = managedType.GetElementType();
+            var cppElementType = cppArray.Type.BaseType.GetGenericArguments()[0];
 
-            if (managedElementType.IsValueType)
+            if (cppElementType == managedElementType)
             {
-                Type cppElementType = cppList.Type.GetGenericArguments()[0];
-
-                MethodInfo convertStructList = ((Func<ILCollections.List<int>, int[]>)ConvertStructList<int, int>)
-                    .Method.GetGenericMethodDefinition().MakeGenericMethod(managedElementType, cppElementType);
-
-                return Expression.Call(convertStructList, cppList);
+                return Expression.Call(((Func<Il2CppArrayBase<float>, float[]>)ConvertArray).Method.GetGenericMethodDefinition().MakeGenericMethod(managedElementType), cppArray);
             }
-            else
+            else if (managedElementType.IsValueType)
             {
-                MethodInfo convertGeneralList = ((Func<ILCollections.List<int>, int[]>)ConvertGeneralList)
-                    .Method.GetGenericMethodDefinition().MakeGenericMethod(managedElementType);
-
-                return Expression.Call(convertGeneralList, cppList);
+                return Expression.Call(
+                    ((Func<Il2CppStructArray<float>, float[]>)ConvertStructArray<float, float>).Method.GetGenericMethodDefinition().MakeGenericMethod(managedElementType, cppElementType), cppArray);
             }
+
+            throw new NotImplementedException();
         }
 
-        public static Expression ConvertArrayToIl2CppList(Expression managedArray, Type cppType)
+        public static T[] ConvertArray<T>(Il2CppArrayBase<T> array)
         {
-            Type cppElementType = cppType.GetGenericArguments()[0];
-            Type managedElementType = managedArray.Type.GetElementType();
-
-            Expression cppList;
-
-            if (cppElementType.IsValueType)
-            {
-                MethodInfo convertStructArray = ((Func<int[], ILCollections.List<int>>)ConvertStructArray<int, int>)
-                    .Method.GetGenericMethodDefinition().MakeGenericMethod(managedElementType, cppElementType);
-
-                cppList = Expression.Call(convertStructArray, managedArray);
-            }
-            else
-            {
-                MethodInfo convertGeneralArray = ((Func<int[], ILCollections.List<int>>)ConvertGeneralArray)
-                    .Method.GetGenericMethodDefinition().MakeGenericMethod(managedElementType);
-
-                cppList = Expression.Call(convertGeneralArray, managedArray);
-            }
-
-            var ptr = typeof(Il2CppSystem.Object).GetProperty("Pointer");
-            return Expression.Property(cppList, ptr);
+            var res = new T[array.Length];
+            array.CopyTo(res, 0);
+            return res;
         }
 
-        /// <summary>
-        /// IL2CPP List -> Managed Array
-        /// </summary>
-        private static TManaged[] ConvertStructList<TManaged, TCpp>(ILCollections.List<TCpp> cppList)
-            where TManaged : unmanaged
-            where TCpp : unmanaged
+        public static TM[] ConvertStructArray<TM, TC>(Il2CppStructArray<TC> array) where TM : unmanaged where TC : unmanaged
         {
-            int size = sizeof(TManaged);
-            if (size != sizeof(TCpp)) { throw new ArgumentException("Size mismatch in array copy."); }
-
-            TManaged[] res = new TManaged[cppList.Count];
-            fixed (TManaged* resPtr = res)
+            var res = new TM[array.Length];
+            fixed (TM* destPtr = res)
             {
                 for (int i = 0; i < res.Length; i++)
                 {
-                    *(TCpp*)(resPtr + i) = cppList[i];
+                    *(TC*)(destPtr + i) = array[i];
                 }
             }
 
             return res;
         }
 
-        /// <summary>
-        /// IL2CPP List -> Managed Array
-        /// </summary>
-        private static T[] ConvertGeneralList<T>(ILCollections.List<T> cppList)
+        public static Il2CppStructArray<TC> ConvertArray<TM, TC>(TM[] array) where TM : unmanaged where TC : unmanaged
         {
-            T[] res = new T[cppList.Count];
-            for (int i = 0; i < res.Length; i++)
+            var res = new Il2CppStructArray<TC>(array.Length);
+            
+            fixed (TM* srcPtr = array)
             {
-                res[i] = cppList[i];
-            }
-
-            return res;
-        }
-
-        /// <summary>
-        /// Managed Array -> IL2CPP List
-        /// </summary>
-        public static ILCollections.List<TCpp> ConvertStructArray<TManaged, TCpp>(TManaged[] managedArray)
-            where TManaged : unmanaged
-            where TCpp : unmanaged
-        {
-            var res = new ILCollections.List<TCpp>(managedArray.Length);
-            fixed (TManaged* managedPtr = managedArray)
-            {
-                for (int i = 0; i < managedArray.Length; i++)
+                for (int i = 0; i < array.Length; i++)
                 {
-                    TManaged* ptr = &managedPtr[i];
-                    res.Add(*(TCpp*)ptr);
+                    res[i] = *(TC*)(srcPtr + i);
                 }
-            }
-
-            return res;
-        }
-
-        /// <summary>
-        /// Managed Array -> IL2CPP list
-        /// </summary>
-        public static ILCollections.List<T> ConvertGeneralArray<T>(T[] managedArray)
-        {
-            var res = new ILCollections.List<T>(managedArray.Length);
-
-            for (int i = 0; i < managedArray.Length; i++)
-            {
-                res.Add(managedArray[i]);
             }
 
             return res;
